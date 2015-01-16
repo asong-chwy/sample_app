18be71ca26a59f429d94eb7cc727fe88838c52b3
Amanda Song <asong@constantcontact.com>
2015-01-16 15:08:52 -0500

    fixed issue with defining current_user method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,4 +19,11 @@ class SessionsController < ApplicationController
 	def destroy
 	end
 	
+	def current_user(user)
+		@current_user = user
+	end
+
+	def current_user
+		@current_user ||= User.find_by_remember_token(cookies[:remember_token])
+	end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,14 +1,14 @@
 module SessionsHelper
 	def sign_in(user)
 		cookies.permanent[:remember_token] = user.remember_token
-		@current_user = user
+		self.current_user = user
 	end
 
 	def signed_in?
 		!current_user.nil?
 	end
 
-	def current_user(user)
+	def current_user=(user)
 		@current_user = user
 	end
 
