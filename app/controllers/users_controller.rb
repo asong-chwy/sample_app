d9e7f619e9b0e05d0ff7e92cd14d1b19ad567ff7
Amanda Song <asong@constantcontact.com>
2015-01-26 17:10:31 -0500

    working on successful edit of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,5 +21,14 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      #handle a successful supdate
+    else
+      render 'edit'
+    end
+  end
+
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+#todo, add tests to check for lack of links when user is not signed in
 
 describe "Authentication" do
 
@@ -37,6 +38,7 @@ describe "Authentication" do
 
 			  it { should have_selector('title', text: user.name) }
 			  it { should have_link('Profile', href: user_path(user)) }
+        it { should have_link('Settings', href: edit_user_path(user)) }
 			  it { should have_link('Sign out', href: signout_path) }
 		    it { should_not have_link('Sign in', href: signin_path) }
 
